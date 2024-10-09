0e891980a648251a61dc204ed2b4c473f4e85347
Davi Luiz <aloha.devdl@gmail.com>
2024-10-08 21:56:34 -0400

    update calculadora de IMC
    Ajustes no js

--- IMC/js/script.js
+++ IMC/js/script.js
@@ -1,17 +1,104 @@
 const form = document.querySelector('#form');
-const resultado = document.querySelector('.result');
+const result = document.querySelector('.result');
 
-function resultForm(event){
+form.addEventListener('submit', resultado);
+
+function resultado(event){
     event.preventDefault();
 
     const peso = document.querySelector('#weight').value;
     const altura = document.querySelector('#height').value;
+    const alturaquadrada = altura*altura;
 
-    const imctotal =  (peso/(altura*altura)).toFixed(2);
+    const imctotal = (peso/alturaquadrada).toFixed(2);
 
-    resultado.innerHTML = `<p> imc de ${imctotal} </p>`;
+    result.innerHTML = `seu imc é de ${imctotal}`;
 
-    console.log(resultado);
+    if (imctotal < 16.9){
+        result.innerHTML += `seu IMC está muito a baixo do peso`
+    } else if(imctotal >= 17 && imctotal <= 18.4){
+        result.innerHTML += `seu IMC está baixo do peso`
+    } else if (imctotal >= 18.5 && imctotal <=24.9){
+        result.innerHTML +=`seu IMC está normal`
+    } else if(imctotal >= 25 && imctotal <= 29.9){
+        result.innerHTML +=`seu imc está com Sobrepeso`
+    } else if(imctotal >= 30 && imctotal <= 34.9){
+        result.innerHTML += `seu imc está com Obesidade grau 1`
+    } else if(imctotal >= 35 && imctotal <= 40){
+        result.innerHTML += `seu imc está com Obesidade grau 2`
+    } else{
+        result.innerHTML += `seu imc está com Obesidade grau 3`
+    }
 }
 
-form.addEventListener('submit', resultForm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('#form');
+// const resultado = document.querySelector('.result');
+
+// function resultForm(event){
+//     event.preventDefault();
+
+//     const peso = document.querySelector('#weight').value;
+//     const altura = document.querySelector('#height').value;
+
+//     const imctotal = (peso/(altura*altura)).toFixed(2);
+
+//     resultado.innerHTML = `<p> imc de ${imctotal} </p>`;
+
+//     console.log(resultado);
+// }
+
+// form.addEventListener('submit', resultForm)
