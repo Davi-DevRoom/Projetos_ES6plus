9d6479eba46d3011b745faaa9aa019ba54120317
Davi Luiz <aloha.devdl@gmail.com>
2024-11-28 23:18:08 -0400

    Gerador de Numeros aleatorios

---
+++ geradorDeNumerosAleatorios/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#generate');
+
+btn.addEventListener('click', numeroAleatorio);
+
+function numeroAleatorio(){
+    let numeroMin = parseInt(document.querySelector('#min').value);
+    let numeroMax = parseInt(document.querySelector('#max').value);
+
+    let result = Math.floor(Math.random() * (numeroMax - numeroMin + 1)) + numeroMin;
+    
+    document.querySelector('#result > span').textContent = result;
+}
