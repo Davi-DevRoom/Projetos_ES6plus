66268308fc6f660f8cad4ea343b26bfbd98c716a
Davi Luiz <aloha.devdl@gmail.com>
2025-01-24 17:06:32 -0400

    update css juros compostos

--- projetos/calculadoraDeJurosCompostos/JS/script.js
+++ projetos/calculadoraDeJurosCompostos/JS/script.js
@@ -1,15 +1,18 @@
+// Primeiro passo é declarar a constante, puxando elas do dom
+// através do querySelector 
 const BUTTON = document.getElementById('click');
 
+// Função de callback do botão
 function jurosCompostos(){
     const CAP = Number(document.getElementById('valor-inicial').value);
-    const I_JURO = Number(document.getElementById('taxa').value);
+    const TAXA = Number(document.getElementById('taxa').value)/100;
     const TEMPO = Number(document.getElementById('periodo').value);
     const RES = document.querySelector('.result span');
 
-    let i_Temp = (1 + I_JURO/100) **TEMPO;
+    let montante = CAP * (1 + TAXA)**TEMPO;
 
-    let montante = CAP * i_Temp;
-    RES.textContent = montante.toFixed(2);
+    RES.textContent = `R$ ${montante.toFixed(2)}`;
 }
 
+// Crie um evento que ira executar uma função quando uma ação for executada
 BUTTON.addEventListener('click', jurosCompostos);
