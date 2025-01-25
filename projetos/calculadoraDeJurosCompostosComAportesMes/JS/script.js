44cc7a7ddf943bd3f01431d311210f96f4065acb
Davi Luiz <aloha.devdl@gmail.com>
2025-01-25 10:32:23 -0400

    projeto calculadora de juros composto com aportes
    Criação de pagina, javascript e css
    calculadora de juros compostos com aportes mensais, calculadora financeira.

---
+++ projetos/calculadoraDeJurosCompostosComAportesMes/JS/script.js
@@ -0,0 +1,19 @@
+document.querySelector('#calc').addEventListener('click', ()=>{
+    const CAP = Number(document.querySelector('#valor-inicial').value);
+    const VALUE_MES = Number(document.querySelector('#valor-mensal').value);
+    const TAXA = Number(document.querySelector('#taxa-mes').value);
+    const TEMP = Number(document.querySelector('#periodo').value);
+    const RES = document.querySelector('#result  span');
+    
+    
+
+    let i = TAXA/100;
+
+    let montanteInicial = CAP * Math.pow(1 + i, TEMP);
+
+    let montanteAportes = VALUE_MES *((Math.pow(1 + i, TEMP) - 1)/i);
+
+    let montanteFinal = montanteInicial + montanteAportes;
+    if(montanteFinal === isNaN) alert('coloque valores');
+    RES.textContent = `R$ ${montanteFinal.toFixed(2)}`;
+});
